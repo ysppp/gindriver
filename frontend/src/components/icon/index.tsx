9ff8e91de1de071ef2004a2325c78f17b1dea009
leesin <1462270974@qq.com>
2021-05-07 17:00:19 +0800

    feat: 重命名、删除，移动

---
+++ frontend/src/components/icon/index.tsx
@@ -0,0 +1,7 @@
+import { createFromIconfontCN } from '@ant-design/icons'
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_2525385_chadef2pr2j.js',
+})
+
+export default IconFont
